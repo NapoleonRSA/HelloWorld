10bb3b86bffc6b1830e2d29ccca065b777ed14dc
Danie Schwartz <schwartz.napoleon@gmail.com>
2017-06-11 12:19:45 +0200

    Initial Commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            //int x;
+            //int y;
+            //x = 7;
+            //y = x + 3;
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("Type your first name:");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your Last Name ?");
+            Console.WriteLine("Type your Last Name :");
+
+            string myLastName;
+            myLastName = Console.ReadLine();
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName );
             Console.ReadLine();
         }
     }
