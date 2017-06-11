a9f52607165b2d53c3e46da90a1c297eabf4b267
Danie Schwartz <schwartz.napoleon@gmail.com>
2017-06-11 13:24:19 +0200

    IF statement done

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,32 +10,45 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Bob's Big Giveaway");
+            //Console.Write("Choose a door : 1, 2 or 3 :");
+            //string uservalue = Console.ReadLine();
+            //string message;
+
+            //if (uservalue == "1")
+            //{
+            //     message = "You won a new car !";
+            //    Console.WriteLine(message);
+            //}
+
+            //else if (uservalue == "2")
+            //{
+            //     message = "You won a new boat !";
+            //    Console.WriteLine(message);
+            //}
+            //else if (uservalue == "3")
+            //{
+            //     message = "You won a cat !";
+            //    Console.WriteLine(message);
+            //}
+
+            //else
+            //{
+            //     message = "Sorry we didnt understand";
+            //    Console.WriteLine(message);
+            //    message += " You loose";
+            //}
+            //Console.ReadLine();
+
             Console.WriteLine("Bob's Big Giveaway");
-            Console.Write("Choose a door : 1, 2 or 3 :");
-            string uservalue = Console.ReadLine();
+            Console.Write("Choose a door : 1, 2 or 3 - ");
+            string userValue = Console.ReadLine();
 
-            if (uservalue == "1")
-            {
-                string message = "You won a new car !";
-                Console.WriteLine(message);
-            }
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            //Console.WriteLine("You won a {0}",message);
+            //Console.ReadLine();
 
-            else if (uservalue == "2")
-            {
-                string message = "You won a new boat !";
-                Console.WriteLine(message);
-            }
-            else if (uservalue == "3")
-            {
-                string message = "You won a cat !";
-                Console.WriteLine(message);
-            }
-
-            else
-            {
-                string message = "Sorry we didnt understand";
-                Console.WriteLine(message);
-            }
+            Console.WriteLine("You entered: {0}, therfore you won a {1}",userValue,message);
             Console.ReadLine();
         }
     }
