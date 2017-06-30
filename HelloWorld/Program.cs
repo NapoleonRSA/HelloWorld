f4ac0e675770b1545f40996a33f48c8ab93a21bb
Danie Schwartz <schwartz.napoleon@gmail.com>
2017-06-30 13:43:21 +0200

    Understanding Classes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,25 +6,54 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            DateTime myValue =  DateTime.Now;
-            //Console.WriteLine(myValue.ToString());
-            //Console.WriteLine(myValue.ToShortDateString());
-            //Console.WriteLine(myValue.ToShortTimeString());
-            //Console.WriteLine(myValue.ToLongTimeString());
-            //Console.WriteLine(myValue.AddDays(3).ToShortDateString());
-            //Console.WriteLine(myValue.AddHours(3).ToShortDateString());
-            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
-            //Console.WriteLine(myValue.Month);
+            Car myCar = new Car();
 
-            //DateTime myBirthday = new DateTime(1989, 01, 05);
-            //Console.WriteLine(myBirthday.ToShortDateString());
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
 
-            DateTime myBirhday = DateTime.Parse("01/05/1989");
-            TimeSpan myAge = DateTime.Now.Subtract(myBirhday);
-            Console.WriteLine(myAge.TotalDays);
-
-                Console.ReadLine();
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year, 
+                myCar.Color
+                );
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}",value);
+            Console.WriteLine("{0:C}",myCar.DetermineMarketValue());
+            Console.ReadLine();
         }
+
+        private static decimal DetermineMarketValue(Car myCar)
+        {
+            decimal carValue = 100.0M;
+
+            return carValue;
+        }
+
+
+        //static void Main(string[] args)
+        //{
+        //    DateTime myValue =  DateTime.Now;
+        //    //Console.WriteLine(myValue.ToString());
+        //    //Console.WriteLine(myValue.ToShortDateString());
+        //    //Console.WriteLine(myValue.ToShortTimeString());
+        //    //Console.WriteLine(myValue.ToLongTimeString());
+        //    //Console.WriteLine(myValue.AddDays(3).ToShortDateString());
+        //    //Console.WriteLine(myValue.AddHours(3).ToShortDateString());
+        //    //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+        //    //Console.WriteLine(myValue.Month);
+
+        //    //DateTime myBirthday = new DateTime(1989, 01, 05);
+        //    //Console.WriteLine(myBirthday.ToShortDateString());
+
+        //    DateTime myBirhday = DateTime.Parse("01/05/1989");
+        //    TimeSpan myAge = DateTime.Now.Subtract(myBirhday);
+        //    Console.WriteLine(myAge.TotalDays);
+
+        //        Console.ReadLine();
+        //}
         //static void Main(string[] args)
         //{
         //    //string myString = "My \"so-called\" life ";
@@ -131,5 +160,27 @@ namespace HelloWorld
         ////    } while (incorrect);
         ////    Console.WriteLine("Correct! It took you {0} guesses",guesses);
         ////}
+    }
+
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 1990)
+                carValue = 10000;
+            else
+            {
+                carValue = 2000;
+            }
+
+            return carValue;
+        }
     }
 }
